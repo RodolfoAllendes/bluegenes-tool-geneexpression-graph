06723f4c1a024b9e1ae04cb6c1336ca975e2ffd5
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2021-06-22 15:44:44 +0900

    Added query for the right type of Gene Expression Data (hbiExpression).

--- src/index.js
+++ src/index.js
@@ -1,32 +1,57 @@
-// add any imports if needed, or write your script directly in this file.
-// const SomePackage = require('PackageName');
+'use strict';
+import { GeneExpressionGraph } from './GeneExpressionGraph.mjs';
 
 // make sure to export main, with the signature
-function main(el, service, imEntity, state, config, navigate) {
+export function main(el, service, imEntity, state, config) {
 	if (!state) state = {};
 	if (!el || !service || !imEntity || !state || !config) {
 		throw new Error('Call main with correct signature');
 	}
-	// Tips to ensure your tool works correctly in BlueGenes:
-	// - The `el` element is all yours, but please do not manipulate the DOM above this
-	// - Avoid ambiguous query selectors that may end up returning an element not belonging to this tool
-	// - Avoid relying on element IDs, as multiple instances of this tool may be present on one page
-
-	/* Example - you can delete this and replace with your own code *******
-
-		// Sample code here to convert the provided InterMine object ID into the data the tool needs.
-		var entity = imEntity.Gene;
-		var mine = new imjs.Service(service);
-		mine.findById(entity.class, entity.value).then(function(obj) {
-			console.log(obj.name + ' is a ' + obj.class + ' you can find in ' + obj.organism.name);
-		});
-	*/
-
+	// define the TargetMine service
+	let tmService = new imjs.Service(service);
+	tmService.fetchModel().then(model => {
+		// retrieve the data from TargetMine using a predefined query
+		let query = new imjs.Query({ model });
+		// from 
+		query.adjustPath('Gene');
+		// select
+		query.select([
+			'Gene.primaryIdentifier',
+			'Gene.symbol',
+			'Gene.probeSets.probeSetId',
+			'Gene.probeSets.expressions.tissue.category',
+			'Gene.probeSets.expressions.tissue.organ',
+			'Gene.probeSets.expressions.tissue.name',
+			'Gene.probeSets.expressions.call',
+			'Gene.probeSets.expressions.value'
+		]);
+		// where
+		query.addConstraints([
+			{
+				path: 'id',
+				op: '=',
+				value: imEntity.Gene.value
+			},
+			{
+				path: 'Gene.probeSets.expressions.tissue',
+				type: 'HbiTissue'
+			},
+			{
+				path: 'Gene.probeSets.expressions',
+				type: 'HbiExpression'
+			}
+		]);
+		return tmService.records(query); 
+	}).then(rows => {
+		window.GeneExpressionGraph = new GeneExpressionGraph(rows);
+	});
+	// create the top-level container for the graph
 	el.innerHTML = `
 		<div class="rootContainer">
 			<h1>Your Data Viz Here</h1>
 		</div>
 	`;
+	
 }
 
-module.exports = { main };
+// export = { main as main};
